c3791519bbf533c1d541a2adc72e66f293b05a86
palle146github <prasanthpalle146@gmail.com>
2023-01-08 01:03:53 +0530

    Employee Daily Wage

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
     public static void main(String[] args) {
-
+            int empDailywage = 0 ;
             int isPresent = 0;
-            isPresent = (int) Math.random()*10%2;
+            isPresent = (int) ( Math.random() * 10 ) % 2;
             int present = 1;
             if( isPresent == present ){
-                System.out.println(" Employee Present");
+                empDailywage  =  WAGE_PER_HOUR * FULL_DAY_HOUR;
             }
             else {
                 System.out.println("Employee is Absent");
             }
-        }
+        System.out.println("Daily Employee Wage ==> "+ empDailywage);
+
+    }
+
     }
