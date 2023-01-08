bf60a3e20d3945c4ffe76581056ba08b83c7b549
palle146github <prasanthpalle146@gmail.com>
2023-01-08 07:41:23 +0530

    Employee daily wage problem

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 public class Main {
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
     public static void main(String[] args) {
             int empDailywage = 0 ;
             int isPresent = 0;
-            isPresent = (int) ( Math.random() * 10 ) % 2;
-            int present = 1;
-            if( isPresent == present ){
+            isPresent = (int) ( Math.random() * 10 ) % 3;
+            int presentFulltime = 1;
+            int presentParttime = 2;
+
+            if( isPresent == presentFulltime ){
                 empDailywage  =  WAGE_PER_HOUR * FULL_DAY_HOUR;
             }
+            else if ( isPresent == presentParttime ){
+            empDailywage  =  WAGE_PER_HOUR * PART_TIME_HOUR;
+            }
+
             else {
                 System.out.println("Employee is Absent");
+
             }
         System.out.println("Daily Employee Wage ==> "+ empDailywage);
 
