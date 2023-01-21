e2aae20fa29cc7adf2ecb0ee340beab0fa832f4b
palle146github <112770521+palle146github@users.noreply.github.com>
2023-01-21 10:04:13 +0530

    Added: Employee Wage Builder to get total for a company

--- src/com/bridgelabz/employeewagecomputation/EmpWageBuilderGettingTotalWage.java
+++ src/com/bridgelabz/employeewagecomputation/EmpWageBuilderGettingTotalWage.java
@@ -1,18 +1,25 @@
 package com.bridgelabz.employeewagecomputation;
 
-import java.util.ArrayList;
+import java.util.*;
 // implementation of interface
 
-public class EmpWageBuilderArrayList implements ICompanyEmpWage{
+public class EmpWageBuilderGettingTotalWage implements ICompanyEmpWage{
     //Constants
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     // Array Declaration
-    ArrayList<CompanyEmpWage> companyEmpWageArray=new ArrayList<CompanyEmpWage>();//Creating arraylist
+    private LinkedList<CompanyEmpWage> companyEmpWageLinkedList = new LinkedList<CompanyEmpWage>();//Creating LinkedList
+    private Map<String, CompanyEmpWage> companyEmpWageMap = new HashMap<String, CompanyEmpWage>();
+       //Creating Map
+
+    public EmpWageBuilderGettingTotalWage(){
+        companyEmpWageLinkedList = new LinkedList<CompanyEmpWage>();
+        companyEmpWageMap = new HashMap<String, CompanyEmpWage>();    }
     @Override // Overriding the addCompnay method in the interface class
     public void addCompany(String company, int empRatePerHour, int numOfWorkingHours, int maxWorkingDays ){
         CompanyEmpWage companyEmpWage =new CompanyEmpWage(company,empRatePerHour,numOfWorkingHours,maxWorkingDays);
-        companyEmpWageArray.add(companyEmpWage);
+        companyEmpWageLinkedList.add(companyEmpWage);
+        companyEmpWageMap.put(company, companyEmpWage);
     }
     // Employee Wage computation
     @Override// Overriding the computeEmpWage method in the interface class
@@ -44,18 +51,24 @@ public class EmpWageBuilderArrayList implements ICompanyEmpWage{
     // Compute employee wage for multiple companies
     @Override// Overriding the computeEmpWage method in the interface class
     public void computeEmpWage(){
-        for( int i = 0; i < companyEmpWageArray.size(); i++){
-            companyEmpWageArray.get(i).setTotalEmpWage((computeEmpWage(companyEmpWageArray.get(i))));
-            System.out.println(companyEmpWageArray.get(i));
+        for( int i = 0; i < companyEmpWageLinkedList.size(); i++){
+            companyEmpWageLinkedList.get(i).setTotalEmpWage((computeEmpWage(companyEmpWageLinkedList.get(i))));
+            System.out.println(companyEmpWageLinkedList.get(i));
         }
     }
+
+    public int getTotalWage(String company){
+        return companyEmpWageMap.get(company).totalEmpWage;
+    }
+
     public static void main(String[] args) {
-        EmpWageBuilderArrayList empWageBuilderArray = new EmpWageBuilderArrayList();// Object declaration
+        EmpWageBuilderGettingTotalWage empWageBuilderArray = new EmpWageBuilderGettingTotalWage();// Object declaration
         empWageBuilderArray.addCompany(" Wipro", 20, 100, 20);
         empWageBuilderArray.addCompany("Infosys", 15, 120, 25);
         empWageBuilderArray.addCompany(" TCS", 22, 110, 20);
         empWageBuilderArray.addCompany("Cognizant", 25, 90, 25);
         empWageBuilderArray.addCompany(" Capgemini", 18, 120, 20);
         empWageBuilderArray.computeEmpWage();
+        System.out.println("Total Employee Wage of Capgemini is " +  empWageBuilderArray.getTotalWage("Capgemini"));
     }
 }
